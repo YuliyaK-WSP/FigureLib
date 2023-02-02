886cdcb3f438a779af9d9758fe41bfc221377991
Julia Koroleva <koroleva21.jv@gmail.com>
2023-02-03 02:25:44 +0300

    Добавление Unit-test

---
+++ Figure.Tests/CFigureTest.cs
@@ -0,0 +1,28 @@
+﻿using FigureLibrary;
+namespace Figure.Tests
+{
+    [TestClass]
+    public class CFigureTest
+    {
+        [TestMethod]
+        public void CircleTest()
+        {
+            CFigure circle = new CFigure(2.0);
+            Assert.AreEqual("Круг", circle.FigureType);
+            Assert.AreEqual(12.57, circle.GetArea());
+        }
+        [TestMethod]
+        public void TriangleTest()
+        {
+            CFigure triangle = new CFigure(2.0, 3.0, 4.0);
+            Assert.AreEqual("Треугольник", triangle.FigureType);
+        }
+        [TestMethod]
+        public void TriangleRightAngleTest()
+        {
+            CFigure triangle = new CFigure(6.0, 8.0, 10.0);
+            Assert.AreEqual("Треугольник", triangle.FigureType);
+            Assert.AreEqual(true, triangle.IsRightAngle());
+        }
+    }
+}
--- FigureLib/Program.cs
+++ FigureLib/Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
+﻿using FigureLibrary;
+ class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CFigure circle = new CFigure(3.0);
+        Console.WriteLine($"Создан объект типа {circle.FigureType}");
+        Console.WriteLine($"Площадь круга {circle.GetArea()}");
+
+        try
+        {
+            CFigure triangle = new CFigure(6.0,8.0,10.0);
+            Console.WriteLine($"Создан объект типа {triangle.FigureType}");
+            Console.WriteLine($"Площадь треугольника {triangle.GetArea()}");
+            if (triangle.IsRightAngle())
+            {
+                Console.WriteLine("Треугольник является прямоугольным");
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не является прямоугольным");
+            }
+
+        }catch(Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
     }
 }
