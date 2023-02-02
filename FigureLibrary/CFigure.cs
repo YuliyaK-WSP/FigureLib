367a77fdd0725137cd4f28dc12b88f206582b2e6
Julia Koroleva <koroleva21.jv@gmail.com>
2023-02-03 01:15:44 +0300

    Добавление функционала в библиотеку Расчет площади круга расчет площади треугольника (по формуле Герона)

--- FigureLibrary/CFigure.cs
+++ FigureLibrary/CFigure.cs
@@ -1,7 +1,51 @@
 ﻿namespace FigureLibrary
 {
-    public class Class1
+    public class CFigure
     {
+       public string FigureType { get; set; }
+        double Radius { get; set; }
+        double SideA { get; set; }
+        double SideB { get; set; }
+        double SideC { get; set; }
 
+        // Конструктор круга
+        public CFigure(double r)
+        {
+            Radius = r;
+            FigureType = "Круг";
+        }
+        //Конструктор треугольника
+
+        public CFigure(double sideA, double sideB, double sideC)
+        {
+            if((sideA + sideB) < sideC ||
+                (sideA + sideC) < sideB ||
+                (sideB + sideC) < sideA)
+            {
+                throw new ArgumentException("Одна сторона треугольника больше суммы двух других сторон. Такой треугольник не может существовать.");
+            }
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+            FigureType = "Треугольник";
+        }
+        // Расчет площади фигуры
+        public double GetArea()
+        {
+
+            if (FigureType == "Круг")
+            {
+                // Вычилсяем площадь круга
+                return Math.Round(Math.PI * Math.Pow(Radius, 2), 2);
+            }
+            else
+            {
+                // Вычисляем площадь треугольника по формуле Герона
+                double p; // Полупериметр
+                p = (SideA + SideB + SideC) / 2;
+                return Math.Round(Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC)), 2);
+            }
+        }
+        
     }
 }
